cb180ab3e1ed3b12354ad78f573df05a230cf676
Hristiyan Velikov <135253187+Hristiyan123@users.noreply.github.com>
2023-12-31 23:00:54 +0200

    Update script.js

--- aboutUs/script.js
+++ aboutUs/script.js
@@ -1,36 +1,13 @@
+// Get the elements
+const signInLink = document.querySelector('.singIn');
+const signUpLink = document.querySelector('.singUp');
+const registrationWindow = document.querySelector('.registration-window');
 
-//activate pop-up
+// Add click event listeners
+signInLink.addEventListener('click', toggleRegistrationWindow);
+signUpLink.addEventListener('click', toggleRegistrationWindow);
 
-let wrapper = document.querySelector('.wrapper');
-let loginLink = document.querySelector('.singIn');
-let registerLink = document.querySelector('.singUp');
-
-function showSignUpPopup() {
-  console.log("Sing-up");
-  wrapper.classList.add('active');
-  registerLink.removeEventListener('click', showSignUpPopup);
-  loginLink.addEventListener('click', showLoginPopup);
+// Function to toggle the registration window visibility
+function toggleRegistrationWindow() {
+  registrationWindow.style.display = registrationWindow.style.display === 'none' ? 'block' : 'none';
 }
-
-function showLoginPopup() {
-  console.log("log-in");
-  wrapper.classList.remove('active');
-  loginLink.removeEventListener('click', showLoginPopup);
-  registerLink.addEventListener('click', showSignUpPopup);
-}
-
-registerLink.addEventListener('click', showSignUpPopup);
-loginLink.addEventListener('click', showLoginPopup);
-
-// close pop-up
-let closeButton = document.querySelector('.icon-close');
-closeButton.addEventListener('click', () => {
-  wrapper.classList.remove('active-popup');
-});
-
-
-
-
-
-
-
