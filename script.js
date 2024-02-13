5ea53aa6bbfaf004770c87a3abb07e730ce0172f
Hristiyan Velikov <135253187+Hristiyan123@users.noreply.github.com>
2024-02-13 17:19:35 +0200

    switch pop-ups

--- script.js
+++ script.js
@@ -1,34 +1,37 @@
-//activate pop-up
-
+let loginBtn = document.querySelector('.loginBtn');
+let registerBtn = document.querySelector('.registerBtn');
 let wrapper = document.querySelector('.wrapper');
-let loginLink = document.querySelector('.singIn');
-let registerLink = document.querySelector('.singUp');
+let loginLink = document.querySelector('.login-link');
+let registerLink = document.querySelector('.register-link');
+let buttonRegister = document.querySelector('.register'); 
 
-function showSignUpPopup() {
-  console.log("Sing-up");
+registerLink.addEventListener('click', () => {
   wrapper.classList.add('active');
-  registerLink.removeEventListener('click', showSignUpPopup);
-  loginLink.addEventListener('click', showLoginPopup);
-}
+});
 
-function showLoginPopup() {
-  console.log("log-in");
+loginLink.addEventListener('click', () => {
   wrapper.classList.remove('active');
-  loginLink.removeEventListener('click', showLoginPopup);
-  registerLink.addEventListener('click', showSignUpPopup);
-}
+});
 
-registerLink.addEventListener('click', showSignUpPopup);
-loginLink.addEventListener('click', showLoginPopup);
+buttonRegister.addEventListener('click', () => {
+  wrapper.classList.add('active-popup');
+});
+
+// Function to switch to login form
+document.getElementById("switchToLogin").addEventListener("click", function () {
+  document.querySelector('.registerTologIn').style.display = "none";
+  document.querySelector('.loginToregister').style.display = "block";
+});
+
+// Function to switch to register form
+document.getElementById("switchToRegister").addEventListener("click", function () {
+  document.querySelector('.loginToregister').style.display = "none";
+  document.querySelector('.registerTologIn').style.display = "block";
+});
 
 // close pop-up
 let closeButton = document.querySelector('.icon-close');
+
 closeButton.addEventListener('click', () => {
   wrapper.classList.remove('active-popup');
 });
-
-
-
-
-
-
