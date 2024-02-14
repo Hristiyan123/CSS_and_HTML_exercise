06be5baba21d1e46a934133483c7af44329f9af5
Hristiyan Velikov <135253187+Hristiyan123@users.noreply.github.com>
2024-02-14 17:36:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,40 @@
-let loginBtn = document.querySelector('.loginBtn');
-let registerBtn = document.querySelector('.registerBtn');
+let loginBtn = document.querySelector('#loginBtn');
+let registerBtn = document.querySelector('#registerBtn');
 let wrapper = document.querySelector('.wrapper');
 let loginLink = document.querySelector('.login-link');
 let registerLink = document.querySelector('.register-link');
-let buttonRegister = document.querySelector('.register'); 
 
-registerLink.addEventListener('click', () => {
-  wrapper.classList.add('active');
-});
-
-loginLink.addEventListener('click', () => {
-  wrapper.classList.remove('active');
-});
-
-buttonRegister.addEventListener('click', () => {
+// login pop-up
+loginBtn.addEventListener('click', () => {
   wrapper.classList.add('active-popup');
+  document.querySelector('.from-box.login').style.display = 'block';
+  document.querySelector('.from-box.rregister').style.display = 'none';
 });
 
-// Function to switch to login form
+// register pop-up
+registerBtn.addEventListener('click', () => {
+  wrapper.classList.add('active-popup');
+  document.querySelector('.from-box.rregister').style.display = 'block';
+  document.querySelector('.from-box.login').style.display = 'none';
+});
+
+// switch to login form
 document.getElementById("switchToLogin").addEventListener("click", function () {
   document.querySelector('.registerTologIn').style.display = "none";
   document.querySelector('.loginToregister').style.display = "block";
+  document.querySelector('.from-box.login').style.display = 'block';
+  document.querySelector('.from-box.rregister').style.display = 'none';
 });
 
-// Function to switch to register form
+// switch to register form
 document.getElementById("switchToRegister").addEventListener("click", function () {
   document.querySelector('.loginToregister').style.display = "none";
   document.querySelector('.registerTologIn').style.display = "block";
+  document.querySelector('.from-box.rregister').style.display = 'block';
+  document.querySelector('.from-box.login').style.display = 'none';
 });
 
-// close pop-up
+// Close pop-up
 let closeButton = document.querySelector('.icon-close');
 
 closeButton.addEventListener('click', () => {
